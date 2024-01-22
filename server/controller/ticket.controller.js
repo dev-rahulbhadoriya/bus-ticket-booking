30d8493ee4b525eaf451e426c5e10dce165afc64
dev-rahulbhadoriya <rbhadoria97@gmail.com>
2024-01-22 21:11:13 +0530

    ADD: ticket controller

---
+++ server/controller/ticket.controller.js
@@ -0,0 +1,11 @@
+const db = require("../models");
+const ticket = db.ticket;
+const ticketServices = require("../service/ticket.service");
+const httpStatus = require("http-status");
+const catchAsync = require("../utils/catchAsync");
+
+// TODO: create ticket controller 
+
+module.exports = {
+    
+}
