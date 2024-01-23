1535627d4d96ae7c1ad35e9cfb45d7f80cd78574
dev-rahulbhadoriya <rbhadoria97@gmail.com>
2024-01-23 20:33:01 +0530

    ADD: adding ticket controller

---
+++ server/route/v1/ticket.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const validate = require('../../middleware/validate');
+
+router
+    .route('/')
+    
+    // TODO: create ticket routes
+
+
+
+module.exports = router;
+
+
+//TODO: swagger 
